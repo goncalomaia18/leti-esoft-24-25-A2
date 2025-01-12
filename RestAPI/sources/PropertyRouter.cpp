9e8ea4b09a5de233756575399990581b93380265
louis <1221432@isep.ipp.pt>
2025-01-12 16:35:14 +0000

    restapi

--- RestAPI/sources/PropertyRouter.cpp
+++ RestAPI/sources/PropertyRouter.cpp
@@ -3,6 +3,10 @@
 void PropertyRouter::configure(httplib::Server &svr) {
     PropertyController propertyController;
 
+    svr.Get("/", [&](const httplib::Request &, httplib::Response &res) {
+        res.set_content("<h1>Welcome to the API</h1><p>Use /properties to get the list of properties.</p>", "text/html");
+    });
+
     svr.Get("/properties", [&](const httplib::Request &, httplib::Response &res) {
         res.set_content(propertyController.getAllProperties(), "application/json");
     });
